032b76493002757fdfc50dff00907428617c78a7
jooelmoel199-arch <jooelmoel199@gmail.com>
2026-09-18 16:05:30 +1000

    Initialize NPC combat queue

--- moba/factory.ts
+++ moba/factory.ts
@@ -139,6 +139,7 @@ export function createPrototypeState(): SimulationState {
     projectiles: [],
     pendingHits: [],
     pendingHitSequence: 0,
+    pendingNpcHits: [],
     pidOrder: players.map(player => player.id),
     nextPidShuffleTick: 45,
     jungleCamps: [
